44c7bc02408e71a32dbeb3478b8b1ae7b4e317a9
BytteM3 <mateo666@live.com>
2025-08-10 02:24:29 +0200

    recursive copy working

--- src/main.py
+++ src/main.py
@@ -1,8 +1,30 @@
-from textnode import TextNode, TextType
+import os
+import shutil
 
+
+def main_copy_func(source, target):
+    if os.path.exists(target):
+        shutil.rmtree(target)
+        os.mkdir(target)
+    else:
+        os.mkdir(target)
+    recursive_copy(source, target)
+
+def recursive_copy(source, target):
+    items_to_copy = os.listdir(source)
+    for item in items_to_copy:
+        item_path = os.path.join(source, item)
+        if os.path.isfile(item_path):
+            shutil.copy(item_path, os.path.join(target, item))
+        elif os.path.isdir(item_path):
+            new_target = os.path.join(target, item)
+            new_source = os.path.join(source, item)
+            os.mkdir(new_target)
+            recursive_copy(new_source, new_target)
+                
 def main():
-    new_node = TextNode("some random ass text", TextType.PLAIN)
-    print(new_node)
+    main_copy_func("static", "public")
 
+if __name__ == "__main__":
+    main()
 
-main()
